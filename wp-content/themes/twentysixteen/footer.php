663a1130364b494f2bdd64c9e33131b2639489d4
mayank <mayank.rathava@qdata.io>
2017-01-18 12:18:14 +0530

    Select 2 js integration-5

--- wp-content/themes/twentysixteen/footer.php
+++ wp-content/themes/twentysixteen/footer.php
@@ -78,7 +78,7 @@ jQuery(document).ready(function() {
   // jQuery('.f1 .dhvc-form-input input').focusout( function() {
   //     jQuery('.f1 span#dhvc_form_control_name-error').hide();
   // });
-      /*jQuery(".add-class-select .dhvc-form-select .dhvc-form-control-city").addClass("js-example-basic-multiple");*/
+      jQuery(".add-class-select .dhvc-form-select .dhvc-form-control-city").addClass("js-example-basic-multiple");
       jQuery(".js-example-basic-single").select2();
      
      jQuery('.scrollbox').enscroll();
