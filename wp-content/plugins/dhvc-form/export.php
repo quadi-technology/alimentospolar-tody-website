343cbe2e62c251b1325a2476bbb8f10136e2b2f9
Yecid F Gomez C <yecidgomez@qdata.io>
2017-02-17 16:02:14 -0500

    Add ID to excel report

--- wp-content/plugins/dhvc-form/export.php
+++ wp-content/plugins/dhvc-form/export.php
@@ -78,7 +78,7 @@ if ( isset($_GET['form_id']) && $_GET['form_id'] != '0' )
 
 				//array_unshift($data_row,$entry->form_url,mysql2date( 'd M Y (H:i)',$entry->submitted,true ));
 				// as request we have removed location column form the excel
-				array_unshift($data_row, mysql2date( 'd M Y (H:i)',$entry->submitted,true ));
+				array_unshift($data_row, $entry->id, mysql2date( 'd M Y (H:i)',$entry->submitted,true ));
 				$data_export_line[$i] = $data_row;
 				$i++;
 			}
